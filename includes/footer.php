fc58a248183ce9186768dcf2b47668855d4d8041
ganesh1643 <ganesh.sonti1643@gmail.com>
2022-04-13 13:29:45 +0530

    v2

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <p> .All rights reserved by Sankuri Naveen</p>
+                                </div>
+                            </div>
+                        </div>
